17f15795ab340c4890901e60db608c89ae8584a2
goncharovush <devnull@localhost>
2020-08-11 15:11:46 +0300

    return changes from mikheev

--- python/test_libivcmp.py
+++ python/test_libivcmp.py
@@ -15,7 +15,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCResistor1.currents[i] = c_double(0.5 * CURRENT_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
         SetMinVC(0, 0)
         res = CompareIvc(self.IVCResistor1, self.IVCResistor1)
-        self.assertTrue(res < 0.1)
+        self.assertTrue(res < 0.05)
 
     def test_number_two(self):
         self.IVCOpenCircuit = IvCurve()
@@ -27,7 +27,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCShortCircuit.currents[i] = c_double(CURRENT_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
         SetMinVC(0, 0)
         res = CompareIvc(self.IVCShortCircuit, self.IVCOpenCircuit)
-        self.assertTrue((res - 1.) < 0.1)
+        self.assertTrue((res - 0.99) < 0.05)
 
     def test_number_three(self):
         self.IVCResistor1 = IvCurve()
@@ -38,7 +38,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCResistor2.voltages[i] = c_double(0.47 * VOLTAGE_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
             self.IVCResistor2.currents[i] = c_double(0.63 * CURRENT_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
         res = CompareIvc(self.IVCResistor1, self.IVCResistor2)
-        self.assertTrue((res - 0.18) < 0.1)
+        self.assertTrue((res - 0.25) < 0.05)
 
     def test_number_four(self):
         self.IVCResistor1 = IvCurve()
@@ -50,7 +50,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCCapacitor.currents[i] = CURRENT_AMPL * np.cos(2 * np.pi * i / MAX_NUM_POINTS)
         SetMinVC(0, 0)
         res = CompareIvc(self.IVCResistor1, self.IVCCapacitor)
-        self.assertTrue((res - 1.) < 0.1)
+        self.assertTrue((res - 0.99) < 0.05)
 
     def test_number_five(self):
         self.IVCResistor1 = IvCurve()
@@ -67,7 +67,7 @@ class TestStringMethods(unittest.TestCase):
         SetMinVC(0, 0)
         res1 = CompareIvc(self.IVCResistor1, self.IVCCapacitor)
         res2 = CompareIvc(self.IVCResistor1, self.IVCCapacitor)
-        self.assertTrue((res1 - res2) < 0.1)
+        self.assertTrue((res1 - res2) < 0.05)
         self.assertTrue(res1 > 0)
 
 
