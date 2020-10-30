Changed distances calculation, fixed tests.

python/test_libivcmp_maxdev.py
@@ -47,7 +47,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCResistor2.voltages[i] = c_double(0.47 * VOLTAGE_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
             self.IVCResistor2.currents[i] = c_double(0.63 * CURRENT_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS))
         resV, resC = ComputeMaxDeviations(self.IVCResistor1, self.IVCResistor2)
-        self.assertTrue(((resV - 0.27) < 0.05) and ((resC - 0.13) < 0.05))
+        self.assertTrue(((resV - 0.22) < 0.05) and ((resC - 0.04) < 0.05))
 
     def test_number_five(self):
         self.IVCResistor1 = IvCurve()
@@ -58,7 +58,7 @@ class TestStringMethods(unittest.TestCase):
             self.IVCCapacitor.voltages[i] = VOLTAGE_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS)
             self.IVCCapacitor.currents[i] = CURRENT_AMPL * np.cos(2 * np.pi * i / MAX_NUM_POINTS)
         resV, resC = ComputeMaxDeviations(self.IVCResistor1, self.IVCCapacitor)
-        self.assertTrue(((resV - 1.98) < 0.05) and ((resC - 0.35) < 0.05))
+        self.assertTrue(((resV - 1.98) < 0.05) and ((resC - 0.15) < 0.05))
 
     def test_number_six(self):
         self.IVCResistor1 = IvCurve()
@@ -66,14 +66,12 @@ class TestStringMethods(unittest.TestCase):
         for i in range(MAX_NUM_POINTS):
             self.IVCResistor1.voltages[i] = 0.5 * VOLTAGE_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS)
             self.IVCResistor1.currents[i] = 0.5 * CURRENT_AMPL * np.sin(2 * np.pi * i / MAX_NUM_POINTS)
-
         self.IVCCapacitor.length = 20
         for i in range(self.IVCCapacitor.length):
             self.IVCCapacitor.voltages[i] = VOLTAGE_AMPL * np.sin(2 * np.pi * i / self.IVCCapacitor.length)
             self.IVCCapacitor.currents[i] = CURRENT_AMPL * np.cos(2 * np.pi * i / self.IVCCapacitor.length)
-
         resV, resC = ComputeMaxDeviations(self.IVCResistor1, self.IVCCapacitor)
-        self.assertTrue(((resV - 1.98) < 0.05) and ((resC - 0.35) < 0.05))
+        self.assertTrue(((resV - 1.98) < 0.05) and ((resC - 0.36) < 0.05))
 
 
 if __name__ == "__main__":