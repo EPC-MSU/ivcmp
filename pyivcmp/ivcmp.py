e5ebd0937504ba4f1848fda035135befe1e2697d
timerke <timerke@mail.ru>
2026-08-04 18:05:01 +0300

    Fixed tests workflows #140407

--- pyivcmp/ivcmp.py
+++ pyivcmp/ivcmp.py
@@ -1,12 +1,8 @@
-from ctypes import CDLL, Structure, Array, c_ubyte, c_double, c_size_t, POINTER, pointer
-from platform import system
-import numpy as np
 import logging
 import os
-
-VOLTAGE_AMPL = 12.
-R_CS = 475.
-CURRENT_AMPL = (VOLTAGE_AMPL / R_CS * 1000)
+from ctypes import Array, c_double, c_size_t, c_ubyte, CDLL, POINTER, pointer, Structure
+from platform import system
+import numpy as np
 
 
 def _fullpath_lib(name: str) -> str:
@@ -35,7 +31,7 @@ def _get_dll():
             "ivcmp.dll",
         )
     else:
-        raise NotImplementedError("Unsupported platform {0}".format(system()))
+        raise NotImplementedError("Unsupported platform {}".format(system()))
 
     errors = []
     for path in paths:
@@ -67,7 +63,8 @@ def _normalize_arg(value, desired_ctype):
 
     if isinstance(value, desired_ctype):
         return value
-    elif issubclass(desired_ctype, Array) and isinstance(value, Sequence):
+
+    if issubclass(desired_ctype, Array) and isinstance(value, Sequence):
         member_type = desired_ctype._type_
 
         if desired_ctype._length_ < len(value):
@@ -75,25 +72,25 @@ def _normalize_arg(value, desired_ctype):
 
         if issubclass(member_type, c_ubyte) and isinstance(value, bytes):
             return desired_ctype.from_buffer_copy(value)
-        elif issubclass(member_type, c_ubyte) and isinstance(value, bytearray):
+        if issubclass(member_type, c_ubyte) and isinstance(value, bytearray):
             return value
-        else:
-            return desired_ctype(*value)
-    else:
-        return value
+
+        return desired_ctype(*value)
+
+    return value
 
 
 class IvCurve(_IterableStructure):
     """
-    Класс сигнатуры (ВАХ)
-    voltages - массив напряжений [Вольты]
-    сurrents - массив токов [мА]
+    Класс сигнатуры (ВАХ).
+    voltages - массив напряжений [В].
+    сurrents - массив токов [мА].
     length - количество элементов в массивах voltages и currents (должно быть одинаковым).
-
     """
+
     _fields_ = (
-        ("_voltages", c_double*MAX_NUM_POINTS),
-        ("_currents", c_double*MAX_NUM_POINTS),
+        ("_voltages", c_double * MAX_NUM_POINTS),
+        ("_currents", c_double * MAX_NUM_POINTS),
         ("length", c_size_t)
     )
 
@@ -141,8 +138,7 @@ def SetMinVarVC(min_var_v, min_var_c):
     библиотека может выдать ненулевую степень различия из-за неправильной нормировки.
     Чтобы этого не произошло, установите порог нормировки с помощью данной функции.
     Сохранённые значения будут использоваться для всех последующих сравнений кривых
-    до тех пор пока масштабы не будут обновлены путём вызова данной функции
-    или функции SetMinVarVCFromCurves().
+    до тех пор, пока масштабы не будут обновлены путём вызова данной функции или функции SetMinVarVCFromCurves().
 
     Способы определения порогов масштабирования:
     - Ручной:
@@ -158,9 +154,10 @@ def SetMinVarVC(min_var_v, min_var_c):
     При любом способе определения пороги масштабирования зависят от диапазонов измерения.
     Поэтому значения необходимо обновлять при каждом изменении настроек измерителя.
 
-    @param min_var_v Характерный масштаб по напряжению. Единицы измерения: Вольты.
-    @param min_var_c Характерный масштаб по току. Единицы измерения: мА.
+    :param min_var_v: Характерный масштаб по напряжению. Единицы измерения: В.
+    :param min_var_c: Характерный масштаб по току. Единицы измерения: мА.
     """
+
     lib_func = lib.SetMinVarVC
     lib_func.argtype = c_double, c_double
     lib_func(c_double(min_var_v),  c_double(min_var_c))
@@ -175,15 +172,16 @@ def SetMinVarVCFromCurves(open_circuit_iv_curve, short_circuit_iv_curve):
     На основе полученных сигнатур функция определяет характерные масштабы шумов
     и задаёт пороги масштабирования при нормировке.
     Установленные значения будут использоваться для всех последующих сравнений кривых
-    до тех пор пока пороги не будут обновлены путём вызова данной функции
-    или функции SetMinVarVC().
+    до тех пор, пока пороги не будут обновлены путём вызова данной функции или функции SetMinVarVC().
     Данная функция является альтернативой для функции SetMinVarVC().
     Если Вы не знаете уровень шумов, но у Вас есть сигнатуры короткого замыкания
     и разрыва, используйте эту функцию. Если у Вас нет сигнатур или
     Вы хотите произвести оценки самостоятельно, используйте функцию SetMinVarVC().
-    @param open_circuit_iv_curve сигнатура, снятая при разомкнутых щупах (объект типа IvCurve)
-    @param short_circuit_iv_curve сигнатура, снятая при разомкнутых щупах (объект типа IvCurve)
+
+    :param open_circuit_iv_curve: Сигнатура, снятая при разомкнутых щупах (объект типа IvCurve).
+    :param short_circuit_iv_curve: Сигнатура, снятая при разомкнутых щупах (объект типа IvCurve).
     """
+
     lib_func = lib.SetMinVarVCFromCurves
     lib_func.argtype = POINTER(c_double), POINTER(c_double), c_size_t, POINTER(c_double), POINTER(c_double), c_size_t
     lib_func.restype = c_double
@@ -197,7 +195,10 @@ def GetMinVarVC():
     """
     Функция для получения текущих значений порогов масштабирования при нормировке токов и напряжений.
     Подробнее о порогах см. описание функции SetMinVarVC.
+
+    :return: Значения порогов масштабирования напряжения и тока.
     """
+
     min_var_v = c_double()
     min_var_c = c_double()
 
@@ -208,26 +209,27 @@ def GetMinVarVC():
     return min_var_v.value, min_var_c.value
 
 
-def CompareIvc(first_iv_curve, second_iv_curve):
+def CompareIvc(first_iv_curve, second_iv_curve) -> float:
     """
     Функция для сравнения двух сигнатур (ВАХ).
-    Возвращает степень различия в диапазоне [0, 1]
-    (0 - кривые совпадают, 1 - кривые совсем разные).
+    Возвращает степень различия в диапазоне [0, 1] (0 - кривые совпадают, 1 - кривые совсем разные).
     Степень различия соответствует визуальному различию кривых.
     Совпадающими сигнатурами являются те, которые совпадают при наложении.
     Максимальным различием обладают сигнатуры разрыва и короткого замыкания
     (в одной из них меняется только напряжение, в другой меняется только ток).
     Если сигнатуры имеют некоторые общие черты, но не совпадают
-    (например, сопротивления разных номиналов
-    или сопротивление и сопротивление с ёмкостью), степень различия будет промежуточной.
+    (например, сопротивления разных номиналов или сопротивление и сопротивление с ёмкостью),
+    степень различия будет промежуточной.
     Передаваемые массивы токов и напряжений должны иметь одинаковую длину
-    и содержать по одному периоду пробного сигнала
-    (один цикл замкнутой кривой).
-    @param first_iv_curve первая кривая для сравнения (объект типа IvCurve)
-    @param second_iv_curve первая кривая для сравнения (объект типа IvCurve)
+    и содержать по одному периоду пробного сигнала (один цикл замкнутой кривой).
+
+    :param first_iv_curve: Первая кривая для сравнения (объект типа IvCurve).
+    :param second_iv_curve: Вторая кривая для сравнения (объект типа IvCurve).
+    :return: Степень различия.
     """
+
     if first_iv_curve.length == 0 or second_iv_curve.length == 0:
-        raise ValueError("IVCurve length attribute should be explicitly set. And it should not be zero")
+        raise ValueError("IVCurve length attribute should be explicitly set. And it should not be zero.")
 
     lib_func = lib.CompareIVC
     lib_func.argtype = POINTER(c_double), POINTER(c_double), c_size_t, POINTER(c_double), POINTER(c_double), c_size_t
@@ -243,6 +245,10 @@ def CompareIvc(first_iv_curve, second_iv_curve):
 
 
 if __name__ == "__main__":
+    VOLTAGE_AMPL = 12.
+    R_CS = 475.
+    CURRENT_AMPL = (VOLTAGE_AMPL / R_CS * 1000)
+
     iv_curve_1 = IvCurve()
     iv_curve_1.length = MAX_NUM_POINTS
     iv_curve_2 = IvCurve()
@@ -253,7 +259,7 @@ if __name__ == "__main__":
         iv_curve_2.voltages[i] = VOLTAGE_AMPL * np.sin(2 * 3.14 * i / MAX_NUM_POINTS)
         iv_curve_2.currents[i] = CURRENT_AMPL * np.sin(2 * 3.14 * i / MAX_NUM_POINTS)
 
-    # Set cureves scale
+    # Set curves scale
     SetMinVarVC(VOLTAGE_AMPL * 0.03, CURRENT_AMPL * 0.03)
 
     score = CompareIvc(iv_curve_1, iv_curve_2)
